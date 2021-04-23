8e99ddcf3777fe65d757f41b321d9835979fb27c
Muhammad Shoaib <66721620+MuhammadShoaib22@users.noreply.github.com>
2021-04-23 17:00:06 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,74 @@
+// console.log("its works");
+
+
+// ObjectDotAssign
+
+// let car = {
+//        car1:"Corolla",
+//        car2:"Honda",
+//        car3:"Suzuki",
+//        car4:"Hyundai",
+//        car5:"Bmw",
+//        car6:"Audi"
+// };
+
+// let carNames = {
+//         car7:"Mehran",
+//         car8:"Alto",
+//         car9:"Gli",
+//         car10:"Aqua",
+//         bikes:{
+//             bike1:"Honda",
+//             bike2:"Yamaha",
+//             bike3:"China",
+//         }
+// };
+
+// let assignCars = Object.assign({},car,carNames);
+// console.log(assignCars);
+
+// assignCars.bikes.bike1 = "Superpower";
+// console.log(assignCars);
+
+
+// Ternary Operator
+// No nested Ternary can apply
+
+// let age = 20;
+
+// let checkAge = age === 20 && age === 20 ? "Age is ok":"No age is not ok" ;
+// console.log(checkAge);
+
+
+// For in loop
+// For in loop use for getting values from object
+
+// let instituteName = {
+//     i1 : "Saims",
+//     i2 : "Aptech",
+//     i3 : "Saylani",
+//     i4 : "Virtual University",
+//     i5 : "Sindh University",
+//     i6 : "Iqra University"
+// }
+
+// for (x in instituteName){
+//     console.log(instituteName[x]);
+// }
+
+// For off loop
+// for off loop use for itrebale objects i.e Arrays and Strings
+// for off only apply on Arrays and Strings
+
+// let myCars = ["Php","Sql","Javascript","Html"];
+
+// for (y of myCars){
+//     console.log(y);
+// }
+
+// Arrow Function 
+// Arrow function is shorter way to write function in Es6 
+// We don't need return or {} in one line of code in arrow function
+
+// let greet = () => "Hello world";
+// console.log(greet());
